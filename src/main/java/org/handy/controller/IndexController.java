e16f7d83cbbff2ffe3b80d768c231ef4811a9d85
ssondh <blue951208@naver.com>
2025-05-11 22:10:49 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/org/handy/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.handy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("IndexController.home()");
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user1
+  Date: 25. 5. 1.
+  Time: 오후 8:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    메인화면2
+</body>
+</html>
